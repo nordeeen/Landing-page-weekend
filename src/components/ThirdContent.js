d63d9926c7e11cc39aac9464e8b9cc915769c5a7
nordeeen <ahmadnrdnsah@gmail.com>
2022-10-08 19:11:55 +0700

    updated code ThirdContent

--- src/components/ThirdContent.js
+++ src/components/ThirdContent.js
@@ -4,17 +4,17 @@ import heroLeftBottom from 'assets/hero-bottom.png';
 const ThirdContent = () => {
   return (
     <>
-      <div className="bg-black w-full h-auto pt-16">
+      <div className="bg-black w-full h-auto lg:pt-16 pt-2">
         <div className="flex justify-center items-center">
-          <div className="w-[100%] h-[400px] flex justify-center py-8 relative">
+          <div className="w-[100%] lg:h-[400px] h-[560px] flex justify-center py-8 relative">
             <div className="w-[244px] h-[325px] absolute left-0 bottom-0 flex flex-end">
               <img src={heroLeftBottom} alt="hero-left-bottom" />
             </div>
             <div className="w-[600px] h-auto">
-              <h1 className="text-center text-white font-bold text-[32px] mb-6">
+              <h1 className="lg:text-center text-left lg:ml-0 ml-[34px] text-white font-bold text-[32px] mb-6">
                 You’re all set.
               </h1>
-              <p className="text-center text-white text-lg">
+              <p className="lg:text-center text-left lg:ml-0 ml-[34px] lg:w-[600px] w-[340px] text-white text-lg">
                 The wise man therefore always holds in these matters to this
                 principle of selection.
               </p>
