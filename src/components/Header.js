219fcecaf804b0ac57431b5de19d90f2c093c02a
nordeeen <ahmadnrdnsah@gmail.com>
2022-10-08 18:43:40 +0700

    updated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import heroNav from 'assets/hero-nav.png';
 const Header = () => {
   return (
     <>
-      <nav className="w-full h-[67px] bg-white py-3 px-52">
+      <nav className="lg:w-full w-[100px] h-[67px] bg-white py-3 lg:px-52 px-0 lg:ml-0 ml-[20px]">
         <div className="flex justify-start items-center  w-[200px]">
           <div className="w-[33px] h-[33px] mx-2">
             <img src={heroNav} alt="hero" />
